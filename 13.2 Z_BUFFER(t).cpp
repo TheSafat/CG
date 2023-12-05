1d933a378b9d63110606dd5a4c8d27d272913703
thesafat <thesafat@gmail.com>
2023-12-06 05:26:55 +0600

    third commit

---
+++ 13.2 Z_BUFFER(t).cpp
@@ -0,0 +1,59 @@
+ #include<bits/stdc++.h>
+#include<graphics.h>
+
+using namespace std;
+/*
+   void triangle(int x1, int y1, int x2, int y2, int x3, int y3)
+   {
+    line(x1, y1, x2, y2);
+    line(x1, y1, x3, y3);
+    line(x2, y2, x3, y3);
+   }
+*/
+void floodFill(int x, int y, int newColor)
+{
+    if(getpixel(x, y) != newColor)
+    {
+        putpixel(x, y, newColor);
+        floodFill(x+1, y, newColor);
+        floodFill(x, y+1, newColor);
+        floodFill(x-1, y, newColor);
+        floodFill(x, y-1, newColor);
+    }
+}
+
+int main()
+{
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, (char*)"");
+    int z_rec=1, z_circle=0;
+
+    if(z_rec>z_circle)
+    {
+
+
+    setcolor(GREEN);
+    rectangle(180, 100, 250, 200);
+    floodFill(200, 180, GREEN);
+
+    setcolor(RED);
+    circle(195, 170, 50);
+    floodFill(200, 180, RED);
+
+    }
+
+    else
+        {
+        setcolor(RED);
+    circle(195, 170, 50);
+    floodFill(200, 180, RED);
+
+     setcolor(GREEN);
+    rectangle(180, 100, 250, 200);
+    floodFill(200, 180, GREEN);
+    }
+    getch();
+    closegraph();
+
+    return 0;
+}
